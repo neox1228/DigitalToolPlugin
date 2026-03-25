b5d230ba27a757c5049337ccefb0211611c773ca
neow <1844801152@qq.com>
2026-03-25 17:26:27 +0800

    v2.0,重新设计插件架构，并添加一个折线图小示例

---
+++ Plugins/ChartsToolPlugin/Source/ChartsToolPlugin/Public/Core/ChartTypes.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/Object.h"
+#include "ChartTypes.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CHARTSTOOLPLUGIN_API UChartTypes : public UObject
+{
+	GENERATED_BODY()
+};
---
+++ Plugins/ChartsToolPlugin/Source/ChartsToolPlugin/Public/SLineChart.h
@@ -0,0 +1,22 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Widgets/SCompoundWidget.h"
+
+/**
+ * 
+ */
+class CHARTSTOOLPLUGIN_API SLineChart : public SCompoundWidget
+{
+public:
+	SLATE_BEGIN_ARGS(SLineChart)
+		{
+		}
+
+	SLATE_END_ARGS()
+
+	/** Constructs this widget with InArgs */
+	void Construct(const FArguments& InArgs);
+};
