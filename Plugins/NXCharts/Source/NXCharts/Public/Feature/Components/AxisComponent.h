0a1a31b720e5e87ced27faa1adb353aa6627d774
neow <1844801152@qq.com>
2026-08-03 17:56:15 +0800

    添加Axis Component

---
+++ Plugins/NXCharts/Source/NXCharts/Public/Feature/Components/AxisComponent.h
@@ -0,0 +1,25 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "ChartComponent.h"
+#include "AxisComponent.generated.h"
+
+class UCartesianCoordModule;
+
+UENUM(BlueprintType)
+enum class EAxisType : uint8
+{
+	X, // X轴
+	Y // Y轴
+};
+
+UCLASS()
+class NXCHARTS_API UAxisComponent : public UChartComponent
+{
+	GENERATED_BODY()
+	
+	UPROPERTY(EditAnywhere)
+	float AxisMin;
+};
